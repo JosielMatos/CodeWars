ffde15a95ca40ff033820c293055407d15e5410f
Josiel Matos <66519559+JosielMatos@users.noreply.github.com>
2021-02-27 18:17:10 -0300

    challenge4

---
+++ Challenges/challenge4.js
@@ -0,0 +1,17 @@
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+function oddOrEven(array) {
+    array.push(0)
+     const sum = array.reduce((acc, c) => acc + c)
+     if (sum % 2 === 0) {
+       return 'even'
+     }else if (sum % 2 > 0) {
+       return 'odd'
+     } else if (sum % 2 < 0) {
+       return 'odd'
+     }
+  }
