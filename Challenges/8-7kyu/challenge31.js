4da05f87c7cde01f732c489f0af32edb6723c588
Josiel Matos <66519559+JosielMatos@users.noreply.github.com>
2021-03-30 07:56:22 -0300

    challenge32

---
+++ Challenges/8-7kyu/challenge31.js
@@ -0,0 +1,27 @@
+// Quarter of the year
+
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the 
+// second quarter; and month 11 (November), is part of the fourth quarter.
+
+const quarterOf = (month) => {
+    if (month < 4) {
+        return 1
+    } else if (month < 7) {
+        return 2
+    } else if (month < 10) {
+        return 3
+    } else if (month < 13) {
+        return 4
+    }
+}
+
+//Other answers
+
+const quarterOf = (month) => {
+    return month < 4 ? 1 : month < 7 ? 2 : month < 10 ? 3 : 4;
+}
+
+
+const quarterOf = m => Math.ceil(m/3);
