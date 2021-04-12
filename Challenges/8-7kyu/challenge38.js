87fc7effa509776aae0f73f4e3dacfe74d8b46d9
Josiel Matos <66519559+JosielMatos@users.noreply.github.com>
2021-04-12 08:15:17 -0300

    challenge40

---
+++ Challenges/8-7kyu/challenge38.js
@@ -0,0 +1,20 @@
+// Powers of 2
+
+// Complete the function that takes a non-negative integer n as input, and returns a list of 
+// all the powers of 2 with the exponent ranging from 0 to n (inclusive).
+
+// Examples
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+
+function powersOfTwo(n){
+  let arr = []
+  let power
+  for (let i=0; i<=n; i++) {
+    power = 2**i
+    arr.push(power)
+  }
+  return arr
+}
