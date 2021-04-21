5a2995d7a745eeec20ec1d55529bf1c850851a8c
Josiel Matos <66519559+JosielMatos@users.noreply.github.com>
2021-04-21 07:52:36 -0300

    challenge53

---
+++ Challenges/8-7kyu/challenge50.js
@@ -0,0 +1,12 @@
+// Sum Mixed Array
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+
+function sumMix(x){
+    let sum = 0
+    x.forEach(el => sum += Number(el))
+    return sum
+}
